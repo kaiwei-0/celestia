e3d838bfcff83e73b48276598e07e6b2143b531b
kaiwei-0 <kaiwei@brevis.network>
2025-07-15 15:11:00 +0800

    Add pico prover client

--- zkVM/sp1/src/bin/cli.rs
+++ zkVM/sp1/src/bin/cli.rs
@@ -1,22 +1,11 @@
-use std::fs::File;
 use clap::Parser;
 use hex::FromHex;
+use pico_sdk::{client::DefaultProverClient, init_logger};
+use serde::{Deserialize, Serialize};
 use sha2::{Digest, Sha256};
-use sp1_sdk::{ProverClient, SP1Stdin};
-
+use std::fs::File;
+use std::{env, fs};
 use zkvm_common::{KEY_LEN, NONCE_LEN, chacha, std_only::ZkvmOutput};
-
-/// The ELF (executable and linkable format) file for the Succinct RISC-V zkVM.
-/// For reproducible builds, you need `cargo prove --docker`
-#[cfg(feature = "reproducible-elf")]
-pub const CHACHA_ELF: &[u8] = include_bytes!(
-    "../../../../target/elf-compilation/docker/riscv32im-succinct-zkvm-elf/release/chacha-program"
-);
-
-#[cfg(not(feature = "reproducible-elf"))]
-pub const CHACHA_ELF: &[u8] = include_bytes!(
-    "../../../../target/elf-compilation/riscv32im-succinct-zkvm-elf/release/chacha-program"
-);
 
 /// The arguments for the command.
 #[derive(Parser, Debug)]
@@ -37,14 +26,11 @@ struct Args {
     load_inputs: Option<std::path::PathBuf>,
 }
 
-
-use serde::{Deserialize, Serialize};
-
 #[derive(Serialize, Deserialize)]
-struct ZkvmInput {
-    key: [u8; KEY_LEN],
-    nonce: [u8; NONCE_LEN],
-    plaintext: Vec<u8>,
+pub struct ZkvmInput {
+    pub key: [u8; KEY_LEN],
+    pub nonce: [u8; NONCE_LEN],
+    pub plaintext: Vec<u8>,
 }
 
 pub fn save_inputs_bin(
@@ -53,7 +39,11 @@ pub fn save_inputs_bin(
     nonce: [u8; NONCE_LEN],
     plaintext: &[u8],
 ) -> bincode::Result<()> {
-    let input = ZkvmInput { key, nonce, plaintext: plaintext.to_vec() };
+    let input = ZkvmInput {
+        key,
+        nonce,
+        plaintext: plaintext.to_vec(),
+    };
     let mut f = File::create(path)?;
     bincode::serialize_into(&mut f, &input)?;
     Ok(())
@@ -63,10 +53,23 @@ pub fn load_inputs_bin(path: &std::path::Path) -> bincode::Result<ZkvmInput> {
     bincode::deserialize_from(File::open(path)?)
 }
 
+/// Loads an ELF file from the specified path.
+pub fn load_elf(path: &str) -> Vec<u8> {
+    fs::read(path).unwrap_or_else(|err| {
+        panic!("Failed to load ELF file from {}: {}", path, err);
+    })
+}
+
 fn main() {
     // Setup the logger.
-    sp1_sdk::utils::setup_logger();
+    init_logger();
     dotenv::dotenv().ok();
+
+    // Initialize Pico prover client
+    let elf = load_elf("./program-chacha/elf/riscv32im-pico-zkvm-elf");
+    let client = DefaultProverClient::new(&elf);
+    // Initialize new stdin
+    let mut stdin_builder = client.new_stdin_builder();
 
     // Parse the command line arguments.
     let args = Args::parse();
@@ -81,17 +84,17 @@ fn main() {
     // - nonce = 12 bytes (MUST BE UNIQUE - NO REUSE!)
     // - input_plaintext = bytes to encrypt
 
-    let (input_key, input_nonce, plaintext_vec, mut stdin) = if let Some(ref path) = args.load_inputs {
-        let inp = load_inputs_bin(path).unwrap();
-        let mut st = SP1Stdin::new();
-        st.write_slice(&inp.key);
-        st.write_slice(&inp.nonce);
-        st.write_slice(&inp.plaintext);
-        (inp.key, inp.nonce, inp.plaintext, st)
-    } else {
-        let key = <[u8; KEY_LEN]>::from_hex(
-            std::env::var("ENCRYPTION_KEY").expect("Missing ENCRYPTION_KEY env var"),
-        )
+    let (input_key, input_nonce, plaintext_vec, mut stdin) =
+        if let Some(ref path) = args.load_inputs {
+            let inp = load_inputs_bin(path).unwrap();
+            stdin_builder.write_slice(&inp.key);
+            stdin_builder.write_slice(&inp.nonce);
+            stdin_builder.write_slice(&inp.plaintext);
+            (inp.key, inp.nonce, inp.plaintext, stdin_builder)
+        } else {
+            let key = <[u8; KEY_LEN]>::from_hex(
+                std::env::var("ENCRYPTION_KEY").expect("Missing ENCRYPTION_KEY env var"),
+            )
             .unwrap_or_else(|_| {
                 panic!(
                     "ENCRYPTION_KEY must be {} bytes, hex encoded (ex: `1234...abcd`)",
@@ -99,33 +102,31 @@ fn main() {
                 )
             });
 
-        let nonce: [u8; NONCE_LEN] = zkvm_common::random_nonce();
-        const PLAINTEXT: &[u8] = include_bytes!("../../../static/proof_input_example.bin");
+            let nonce: [u8; NONCE_LEN] = zkvm_common::random_nonce();
+            const PLAINTEXT: &[u8] = include_bytes!("../../../static/proof_input_example.bin");
 
-        let mut st = SP1Stdin::new();
-        st.write_slice(&key);
-        st.write_slice(&nonce);
-        st.write_slice(PLAINTEXT);
+            // let mut st = SP1Stdin::new();
+            stdin_builder.write_slice(&key);
+            stdin_builder.write_slice(&nonce);
+            stdin_builder.write_slice(PLAINTEXT);
 
-        if let Some(ref path) = args.save_inputs {
-            save_inputs_bin(path, key, nonce, PLAINTEXT).unwrap();
-            println!("✔️  inputs saved to {}", path.display());
-        }
-        (key, nonce, PLAINTEXT.to_vec(), st)
-    };
+            if let Some(ref path) = args.save_inputs {
+                save_inputs_bin(path, key, nonce, PLAINTEXT).unwrap();
+                println!("✔️  inputs saved to {}", path.display());
+            }
+            (key, nonce, PLAINTEXT.to_vec(), stdin_builder)
+        };
 
-    let client = ProverClient::from_env();
     if args.execute {
         // Execute the program
-        let (output_buffer, report) = client.execute(CHACHA_ELF, &stdin).run().unwrap();
+        let (cycles, pv_stream) = client.emulate(stdin);
 
         // Read the output.
         // - privkey sha2 hash = 32 bytes
         // - nonce = 12 bytes
         // - plaintext sha2 hash = 32 bytes
         // - ciphertext = encrypted bytes, ~1M bytes
-        let output =
-            ZkvmOutput::from_bytes(output_buffer.as_slice()).expect("Failed to parse header");
+        let output = ZkvmOutput::from_bytes(pv_stream.as_slice()).expect("Failed to parse header");
 
         // Check against the input
         let input_plaintext_digest = Sha256::digest(&plaintext_vec);
@@ -148,25 +149,20 @@ fn main() {
         println!("Key used matched!");
 
         // Record the number of cycles executed.
-        println!("Number of cycles: {}", report.total_instruction_count());
+        println!("Number of cycles: {}", cycles);
     } else {
-        // Setup the program for proving.
-        let (pk, vk) = client.setup(CHACHA_ELF);
+        // Set up output path
+        let current_dir = env::current_dir().expect("Failed to get current directory");
+        let output_path = current_dir.join("./test_data");
 
-        // Generate the proof
-        //
-        // NOTE:
-        // Using the [groth16 proof type](https://docs.succinct.xyz/docs/sp1/generating-proofs/proof-types#groth16-recommended) to trade increased proving costs & time for minimal EVM gas costs.
-        let proof = client
-            .prove(&pk, &stdin)
-            .groth16()
-            .run()
-            .expect("failed to generate proof");
+        // Set up groth16 verifier and generate pico proof
+        // The first parameter `need_setup = true` ensures the Groth16 verifier is set up,
+        // but this setup is required only once.
+        let need_setup = true;
+        client
+            .prove_evm(stdin, need_setup, output_path.clone(), "kb")
+            .expect("Failed to generate evm proof");
 
         println!("Successfully generated proof!");
-
-        // Verify the proof.
-        client.verify(&proof, &vk).expect("failed to verify proof");
-        println!("Successfully verified proof!");
     }
 }
